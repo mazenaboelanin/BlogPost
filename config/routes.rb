56730bc5c8ef6ce339ebaf1ec1284f6fae4ee9ad
Mazen <mazenaboelanin@gmail.com>
2022-01-30 23:59:16 +0200

    adding styles with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "posts#index"
+  root "posts#index", as: "home"
 
   # Regular Route
-  get 'about' => 'pages#about'
+  get 'about' => 'pages#about', as: 'about'
 
   # Resource Route 
   resources :posts
